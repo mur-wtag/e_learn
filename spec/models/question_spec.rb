952bfa8fb4ddda1d8157f28ab9e375a9cc7e2286
rony <code.rony36@gmail.com>
2021-02-16 17:41:44 +0600

    - validation added for maximum number of question - spec added for validation

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -18,4 +18,32 @@ RSpec.describe Question, type: :model do
     subject.content = nil
     expect(subject).to_not be_valid
   end
+
+  describe 'validation of maximum number of questions to the lesson' do
+    let(:lesson) { create(:lesson) }
+
+    context 'created more than 10 question' do
+      before do
+        create_list(:question, 10, lesson: lesson)
+        @question = Question.new(lesson_id: lesson.id, content: 'some text')
+        @question.valid?
+      end
+
+      it 'raises an error' do
+        expect(@question.errors.full_messages).to include("A lesson can't have more than 10 questions")
+      end
+    end
+
+    context 'created less than 10 question' do
+      before do
+        create_list(:question, 9, lesson: lesson)
+        @question = Question.new(lesson_id: lesson.id, content: 'some text')
+        @question.valid?
+      end
+
+      it 'should not raise validation error' do
+        expect(@question.errors.full_messages).to be_empty
+      end
+    end
+  end
 end
